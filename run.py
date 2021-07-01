bf416d3544c6abf4b315594b2b5a734a2e1450a0
andrea <andygaspar@libero.it>
2021-07-01 12:18:48 +0200

    added saturation

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from Acc import acc
+import pandas as pd
 
 acc_dict = acc.make_acc_dict()
 
 lovv = acc_dict["LOVVCTA"]
 lovv.plot_config()
+
+df_sat = pd.read_csv("RowData/ACCs_ActualCapacities/Saturations.csv", sep=";")
+
