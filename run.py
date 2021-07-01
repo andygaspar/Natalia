e7cd2f604e345d811ffa3c0e41e2caca806d3371
andrea <andygaspar@libero.it>
2021-07-01 15:38:57 +0200

    ultimo commit

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 from Acc import acc
 import pandas as pd
+from RowData.configuration_data import set_configuration_df
 
 acc_dict = acc.make_acc_dict()
 
-lovv = acc_dict["LOVVCTA"]
-lovv.plot_config()
+
+df_open = set_configuration_df()
 
 df_sat = pd.read_csv("RowData/ACCs_ActualCapacities/Saturations.csv", sep=";")
+
+
+df_occ = pd.read_csv("RowData/ACCs_ActualCapacities/Occupancies.csv", sep=";")
 
