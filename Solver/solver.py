4663fb2d5fcdcae7cb7b1e85e56f526643a95ba9
andrea <andygaspar@libero.it>
2021-09-07 11:19:21 +0200

    inizio implementazione modello

--- Solver/solver.py
+++ Solver/solver.py
@@ -1,37 +1,40 @@
 from itertools import combinations
 import xpress as xp
+from typing import List
+from ACC.acc import Acc, DailyConfiguration
 
 import numpy as np
 
 
 class Solver:
 
-    def __init__(self, accs, intervals):
+    def __init__(self, accs: List[Acc], intervals):
         self.accs = accs
         self.accsNum = len(accs)
         self.intervals = intervals
-        self.intNum = len(intervals)
+        self.intervalsNum = len(intervals)
         self.matches = np.array(list(combinations(self.accs, 2)))
 
         self.p = xp.problem()
-        self.x = np.array([[[xp.var(vartype=xp.binary) for _ in intervals] for _ in accs] for _ in self.accs])
+        self.d = np.array([[[xp.var(vartype=xp.integer) for _ in intervals] for _ in self.accs] for _ in self.accs])
         self.m = np.array([xp.var(vartype=xp.binary) for _ in self.matches])
 
-        self.p.addVariable(self.x, self.m)
+        self.p.addVariable(self.d, self.m)
 
     def set_constraints(self):
         # t is the index of the time period
 
         for acc in self.accs:
             # no self colab
-            for t in range(self.intNum):
-                self.p.addConstraint(self.x[acc.index, acc.index, t] == 0)
+            for t in range(self.intervalsNum):
+                # if acc.
+                self.p.addConstraint(xp.Sum(self.d[acc.index, acc.index, t]) == 0)
 
             self.p.addConstraint(xp.Sum(self.m[k] for k in self.get_acc_matches(acc)) <= 1)
 
         # colab with only one for each interval
         for acc_A in self.accs:
-            for t in range(self.intNum):
+            for t in range(self.intervalsNum):
                 self.p.addConstraint(
                     xp.Sum(self.x[acc_A.index, acc_B.index, t] for acc_B in self.accs) <= 1
                 )
@@ -40,7 +43,7 @@ class Solver:
         for match in self.matches:
             acc_A, acc_B = match[0], match[1]
             self.p.addConstraint(
-                xp.Sum(self.x[acc_A.index, acc_B.index, t] for t in range(self.intNum)) <= self.m[k]
+                xp.Sum(self.x[acc_A.index, acc_B.index, t] for t in range(self.intervalsNum)) <= self.m[k]
             )
             k += 1
 
